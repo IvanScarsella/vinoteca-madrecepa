90a149f7dddbe8f7ff5e0b609bb9b9f75f9b39b2
Iván Scarsella <scarsellaivan@gmail.com>
2024-02-28 17:40:52 -0300

    ...

--- pages/api/createProduct/index.ts
+++ pages/api/createProduct/index.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../prisma/client'
-
+//
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
    if (req.method === "GET") {
       // if (Object.keys(req.body).length === 0) {
